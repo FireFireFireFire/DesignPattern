f161f81ec78e145f1d2d9bbeb2e1c7594513306a
FireFireFireFire <13xszheng2@stu.edu.cn>
2017-03-18 11:31:08 +0800

    Create BMW.java

---
+++ Factory Patter/FactoryMethod/BMW.java
@@ -0,0 +1,18 @@
+abstract class BMW
+{
+    public BMW(){}
+}
+class BMW320 extends BMW
+{
+    public BMW320()
+    {
+        System.out.println("制造-->BMW320");
+    }
+}
+class BMW523 extends BMW
+{
+    public BMW523()
+    {
+        System.out.println("制造-->BMW523");
+    }
+}
